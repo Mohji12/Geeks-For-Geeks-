3f0a2d17e66f1475eccc2778ef6fd72a8d2e3ab2
Mohji12 <mohangola47@gmail.com>
2024-09-04 10:14:02 +0530

    five commit

---
+++ Week1/day3.py
@@ -0,0 +1,14 @@
+l = [10,12,13,19,24,15,19,21]
+
+for i in l:
+    if i % 2 == 0:
+        print("Even Number")
+    else:
+        print("Odd Number")
+
+for i in l:
+    a = i + 2
+    l.append(a)
+
+
+
